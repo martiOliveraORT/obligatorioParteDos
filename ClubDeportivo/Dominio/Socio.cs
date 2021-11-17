be85c6a5cbed5348969ee1bb73c8faea1a44eae2
martiOliveraORT <so255807@fi365.ort.edu.uy>
2021-11-16 22:48:40 -0300

    Primer conexion con EF

--- ClubDeportivo/Dominio/Socio.cs
+++ ClubDeportivo/Dominio/Socio.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Dominio
 {
+    [Table("Socios")]
     public class Socio
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Cedula { get; set; }
         public string Nombre { get; set; }
         public DateTime FechaNac { get; set; }
---
+++ ClubDeportivo/Repositorio/RepoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Dominio;
+
+namespace Repositorio
+{
+    class RepoContext:DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Socio> Socios { get; set; }
+
+        //Cuando creo una instancia de dbContext, debo pasarle la conection string 
+        public RepoContext(string con) : base(con) { }
+    }
+}
