b9303dc06c770d56576516a6859c8e02ebc1e447
MadMeerkat <aionnoiamt2@gmail.com>
2021-11-20 03:06:22 -0300

    Prueba proyecto api

---
+++ ClubDeportivo/ActividadApi/Controllers/ActividadController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Dominio;
+using Fachada;
+
+namespace ActividadApi.Controllers
+{
+    public class ActividadController : ApiController
+    {
+        //GET api/Busqueda
+        public List<ActividadHorario> BusquedaActividad(string texto, int edad, string dia, int hora)
+        {
+            List<ActividadHorario> lista = null;
+
+            FachadaActividades f = new FachadaActividades();
+
+            lista = f.BuscarActividades(texto, edad, dia, hora);
+
+            return lista;
+        }
+    }
+}
---
+++ ClubDeportivo/ClubDeportivo/Controllers/ActividadesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClubDeportivo.Controllers
+{
+    public class ActividadesController : Controller
+    {
+        // GET: Actividades
+        /*
+        public ActionResult Index()
+        {
+            HttpClient cliente = new HttpClient();
+            Uri uri = new Uri("http://localhost:1234/api/actividad");
+            HttpRequestMessage solicitud = new HttpRequestMessage(HttpMethod.Get, uri);
+            string json = "{nombre:\'fut'}";
+            HttpContent contenido = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            solicitud.Content = contenido;
+            Task<HttpResponseMessage> respuesta = cliente.SendAsync(solicitud);
+            respuesta.Wait();
+            return View();
+
+            if (respuesta.IsSuccessStatusCode)
+            {
+                Task<string> contenidoRespuesta = respuesta.Result.Content.ReadAsStringAsync();
+                contenidoRespuesta.Wait();
+            }
+        }*/
+    }
+}
