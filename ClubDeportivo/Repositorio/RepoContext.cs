1d51eb88eaee9311d96577d8fb5b8fccae21d80c
MadMeerkat <aionnoiamt2@gmail.com>
2021-11-20 02:09:23 -0300

    Fix Cambio en Contexto

--- ClubDeportivo/Repositorio/RepoContext.cs
+++ ClubDeportivo/Repositorio/RepoContext.cs
@@ -12,33 +12,26 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Repositorio
 {
-    class RepoContext:DbContext
+    class RepoContext : DbContext
     {
         public DbSet<Usuario> Usuarios { get; set; }
         public DbSet<Socio> Socios { get; set; }
 
         public DbSet<Mensualidad> Mensualidades { get; set; }
         public DbSet<Generalidades> Generalidades { get; set; }
+        public DbSet<Actividad> Actividades { get; set; }
+        public DbSet<Horario> Horarios { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Mensualidad>()
                 .Map<Cuponera>(m => m.Requires("Tipo").HasValue("c"))
                 .Map<PaseLibre>(m => m.Requires("Tipo").HasValue("l"));
+
+            //A la tabla Horarios, seteamos la key compuesta
+            modelBuilder.Entity<Horario>().HasKey(h => new { h.Actividad, h.Dia, h.Hora });
         }
-
-        public DbSet<Actividad> Actividades { get; set; }
-        public DbSet<Mensualidad> Mensualidades { get; set; }
-        public DbSet<Horario> Horarios { get; set; }
-
 
         //Cuando creo una instancia de dbContext, debo pasarle la conection string 
         public RepoContext(string con) : base(con) { }
-
-        //A la tabla Horarios, seteamos la key compuesta
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Horario>().HasKey(h=> new{h.Actividad, h.Dia, h.Hora});
-        }
-
     }
 }
