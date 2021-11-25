11428ea529505618c3633a8d46dcc905cde4fdb3
martiOliveraORT <so255807@fi365.ort.edu.uy>
2021-11-25 01:12:59 -0300

    Front Ingresos socio completo

---
+++ ClubDeportivo/ApiClubDeportivo/Controllers/RegistroController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Fachada;
+using Dominio;
+
+namespace ApiClubDeportivo.Controllers
+{
+    [RoutePrefix("api/registros")]
+    public class RegistroController : ApiController
+    {
+        [HttpGet]
+        [Route("{ci:int}/{actividad}")] //Get api/registros/"{ci}/{actividad}"
+        public IEnumerable<RegistroActividad> GetIngresosSocio(int ci, string actividad)
+        {
+            FachadaRegistroActividad fachada = new FachadaRegistroActividad();
+            List<RegistroActividad> lista;
+            lista = fachada.IngresoSocioPorActividad(ci, actividad);
+            return lista;
+        }
+    }
+}
---
+++ ClubDeportivo/ClubDeportivo/Controllers/IngresosSocioController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using Fachada;
+using Dominio;
+using System.Linq;
+
+namespace ClubDeportivo.Controllers
+{
+    public class IngresosSocioController : Controller
+    {
+        HttpClient cliente = new HttpClient();
+        HttpResponseMessage response = new HttpResponseMessage();
+        Uri registroUri = null;
+
+        public IngresosSocioController()
+        {
+            cliente.BaseAddress = new Uri("http://localhost:58276/");
+            registroUri = new Uri("http://localhost:58276/api/registros");
+            cliente.DefaultRequestHeaders.Accept.Clear();
+            cliente.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        [HttpGet]
+        public ActionResult BuscarIngresos()
+        {
+            if (Session["Logueado"] == null)
+            {
+                return Redirect("/usuario/Login");
+            }
+            FachadaActividades fachada = new FachadaActividades();
+            List<string> lista = fachada.ListaActividades();
+            ViewBag.lista = lista;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult BuscarIngresosPorActividad(int ci, string actividad)
+        {
+            //Para que arme la lista de actividades
+            FachadaActividades fachada = new FachadaActividades();
+            List<string> lista = fachada.ListaActividades();
+            ViewBag.lista = lista;
+
+            if (Session["Logueado"] == null)
+            {
+                return Redirect("/usuario/Login");
+            }
+            response = cliente.GetAsync(registroUri + "/" + ci + "/" + actividad).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var act = response.Content.ReadAsAsync<IEnumerable<RegistroActividad>>().Result;
+
+                if (act != null && act.Count() > 0)
+                {
+                    ViewBag.m = act.ToList();
+                    return View("BuscarIngresos");
+                }
+            }
+            return View("BuscarIngresos");
+        }
+
+    }
+}
