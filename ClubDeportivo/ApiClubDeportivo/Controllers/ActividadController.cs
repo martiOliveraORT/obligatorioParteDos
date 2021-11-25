aeeeecde2bfa0facb049891caad4dc774d0cff86
martiOliveraORT <so255807@fi365.ort.edu.uy>
2021-11-24 22:55:28 -0300

    Completo web api

---
+++ ClubDeportivo/ApiClubDeportivo/Controllers/ActividadController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Fachada;
+using Dominio;
+
+namespace ApiClubDeportivo.Controllers
+{
+    [RoutePrefix("api/actividades")]
+    public class ActividadController : ApiController
+    {
+        [HttpGet]
+        [Route("{textoEnNombre}/nombre")] //Get api/actividades/{textoEnNombre}/nombre
+        public IEnumerable<ActividadHorario> GetActividadNombre(string textoEnNombre)
+        {
+            FachadaActividades fachada = new FachadaActividades();
+            List<ActividadHorario> lista;
+            lista = fachada.BuscarActividadPorNombre(textoEnNombre);
+            return lista;
+        }
+
+        [HttpGet]
+        [Route("{edad:int}/edad")] //Get api/actividades/{edad}/edad
+        public IEnumerable<ActividadHorario> GetActividadEdad(int edad)
+        {
+            FachadaActividades fachada = new FachadaActividades();
+            List<ActividadHorario> lista;
+            lista = fachada.BuscarActividadPorEdad(edad);
+            return lista;
+        }
+
+        [HttpGet]
+        [Route("{dia}/{hora:int}/dia")] //Get api/actividades/{dia}/{hora}/dia
+        public IEnumerable<ActividadHorario> GetActividadDiaHora(string dia, int hora)
+        {
+            FachadaActividades fachada = new FachadaActividades();
+            List<ActividadHorario> lista;
+            lista = fachada.BuscarActividadPorDiaHora(dia, hora);
+            return lista;
+        }
+    }
+}
--- ClubDeportivo/Fachada/FachadaActividades.cs
+++ ClubDeportivo/Fachada/FachadaActividades.cs
@@ -10,33 +10,27 @@ namespace Fachada
 {
     public class FachadaActividades
     {
-        public List<ActividadHorario> BuscarActividades(string textoEnNombre, int edad, string dia, int hora)
+        public List<ActividadHorario> BuscarActividadPorNombre(string texto)
         {
-            List<ActividadHorario> listadoRetorno = null;
+            List<ActividadHorario> listadoRetorno = new List<ActividadHorario>();
             RepoActividad repo = new RepoActividad();
             List<Horario> listaH = null;
-
-            if (textoEnNombre != "" || textoEnNombre != null)
+            //Filtro para que tipo es la busqueda
+            if (texto != "")
             {
-                listaH = repo.BuscarActividadPorTexto(textoEnNombre);
-            }else if(edad > 0)
-            {
-                listaH = repo.BuscarActividadPorEdad(edad);
-            }else if(dia != "" || dia != null)
-            {
-                listaH = repo.BuscarActividadPorDiaHora(dia, hora);
+                listaH = repo.BuscarActividadPorTexto(texto);
             }
             else
             {
                 return listadoRetorno;
             }
-
-            if(listaH.Count > 0)
+            //Recorro la lista de actividades obtenida, para crear el nuevo 
+            if (listaH.Count > 0)
             {
                 foreach (Horario h in listaH)
                 {
                     Actividad act = repo.BusarPorNombre(h.Actividad);
-                    listadoRetorno.Add(new ActividadHorario
+                    ActividadHorario ah = new ActividadHorario
                     {
                         Nombre = act.Nombre,
                         Id = act.Id,
@@ -46,11 +40,124 @@ namespace Fachada
                         CuposDisponibles = act.CuposDisponibles,
                         Dia = h.Dia,
                         Hora = h.Hora
-                    });
+                    };
+                    try
+                    {
+                        listadoRetorno.Add(ah);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
                 }
             }
-
             return listadoRetorno;
+        }
+
+        public List<ActividadHorario> BuscarActividadPorEdad(int edad)
+        {
+            List<ActividadHorario> listadoRetorno = new List<ActividadHorario>();
+            RepoActividad repo = new RepoActividad();
+            List<Horario> listaH = null;
+            //Filtro para que tipo es la busqueda
+            if (edad > 0)
+            {
+                listaH = repo.BuscarActividadPorEdad(edad);
+            }
+            else
+            {
+                return listadoRetorno;
+            }
+            //Recorro la lista de actividades obtenida, para crear el nuevo 
+            if (listaH.Count > 0)
+            {
+                foreach (Horario h in listaH)
+                {
+                    Actividad act = repo.BusarPorNombre(h.Actividad);
+                    ActividadHorario ah = new ActividadHorario
+                    {
+                        Nombre = act.Nombre,
+                        Id = act.Id,
+                        EdadMin = act.EdadMin,
+                        EdadMax = act.EdadMax,
+                        Duracion = act.Duracion,
+                        CuposDisponibles = act.CuposDisponibles,
+                        Dia = h.Dia,
+                        Hora = h.Hora
+                    };
+                    try
+                    {
+                        listadoRetorno.Add(ah);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+                }
+            }
+            return listadoRetorno;
+        }
+
+        public List<ActividadHorario> BuscarActividadPorDiaHora(string dia, int hora)
+        {
+            List<ActividadHorario> listadoRetorno = new List<ActividadHorario>();
+            RepoActividad repo = new RepoActividad();
+            List<Horario> listaH = null;
+            //Filtro para que tipo es la busqueda
+            if (dia != "")
+            {
+                listaH = repo.BuscarActividadPorDiaHora(dia, hora);
+            }
+            else
+            {
+                return listadoRetorno;
+            }
+            //Recorro la lista de actividades obtenida, para crear el nuevo 
+            if (listaH.Count > 0)
+            {
+                foreach (Horario h in listaH)
+                {
+                    Actividad act = repo.BusarPorNombre(h.Actividad);
+                    ActividadHorario ah = new ActividadHorario
+                    {
+                        Nombre = act.Nombre,
+                        Id = act.Id,
+                        EdadMin = act.EdadMin,
+                        EdadMax = act.EdadMax,
+                        Duracion = act.Duracion,
+                        CuposDisponibles = act.CuposDisponibles,
+                        Dia = h.Dia,
+                        Hora = h.Hora
+                    };
+                    try
+                    {
+                        listadoRetorno.Add(ah);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+                }
+            }
+            return listadoRetorno;
+        }
+
+        public List<string> ListaActividades()
+        {
+            List<string> actividades = new List<string>();
+
+            RepoActividad repo = new RepoActividad();
+
+            List<Actividad> lista = repo.TraerTodo();
+
+            foreach(Actividad a in lista)
+            {
+                actividades.Add(a.Nombre);
+            }
+            return actividades;
         }
         
     }
